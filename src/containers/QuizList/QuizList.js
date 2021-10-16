0a6d0fe8e368a31624e0c60cf2be7635d9106a10
moroz-art-dev <moroz.art.dev@gmail.com>
2021-10-16 14:46:52 +0300

    lessons in blocks_10_1

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import classes from "./QuizList.module.css"
 import {NavLink} from "react-router-dom"
+import axios from "axios"
 
 export default class QuizList extends Component {
     renderQuizes() {
@@ -16,6 +17,13 @@ export default class QuizList extends Component {
             )
         })
     }
+
+    componentDidMount() {
+        axios.get('https://react-quiz-57e56-default-rtdb.firebaseio.com/quiz.json').then(response => {
+            console.log(response)
+        })
+    }
+
     render() {
         return (
             <div className={classes.QuizList}>
