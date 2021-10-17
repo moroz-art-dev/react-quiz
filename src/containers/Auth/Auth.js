f69aa576e680c803595d193daae569b699e1fd85
moroz-art-dev <moroz.art.dev@gmail.com>
2021-10-17 21:39:10 +0300

    lessons in blocks_10_6

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from "./Auth.module.css"
 import Button from "../../components/UI/Button/Button"
 import Input from "../../components/UI/Input/Input"
 import is from "is_js"
+import axios from "axios"
 
 export default class Auth extends Component {
 
@@ -36,34 +37,54 @@ export default class Auth extends Component {
         }
     }
 
-    loginHandler = () => {
-
+    loginHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        }
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDX5fFLBlfFh9NtdP8peaXq_qfo19B-Hw8', authData)
+            console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
     }
 
-    registerHandler = () => {
-
+    registerHandler = async () => {
+        const authData = {
+            email: this.state.formControls.email.value,
+            password: this.state.formControls.password.value,
+            returnSecureToken: true
+        }
+        try {
+            const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDX5fFLBlfFh9NtdP8peaXq_qfo19B-Hw8', authData)
+            console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     submitHandler = event => {
         event.preventDefault()
     }
 
-    validateControl(value, validation){
-        if (!validation){
+    validateControl(value, validation) {
+        if (!validation) {
             return true
         }
 
         let isValid = true
 
-        if (validation.required){
+        if (validation.required) {
             isValid = value.trim() !== '' && isValid
         }
 
-        if (validation.email){
+        if (validation.email) {
             isValid = is.email(value) && isValid
         }
 
-        if (validation.minLength){
+        if (validation.minLength) {
             isValid = value.length >= validation.minLength && isValid
         }
 
@@ -92,7 +113,7 @@ export default class Auth extends Component {
     }
 
     renderInputs() {
-        return Object.keys(this.state.formControls).map((controlName, index)=>{
+        return Object.keys(this.state.formControls).map((controlName, index) => {
             const control = this.state.formControls[controlName]
             return (
                 <Input
