5134523f4c5fa22122a6e544b11c9b1a4f2bb991
moroz-art-dev <moroz.art.dev@gmail.com>
2021-12-15 17:37:29 +0200

    lesson 12_7

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -3,20 +3,13 @@ import classes from "./Drawer.module.css"
 import {NavLink} from 'react-router-dom'
 import Backdrop from "../../UI/Backdrop/Backdrop"
 
-const links = [
-    {to: '/', label: 'Список', exact: true},
-    {to: '/auth', label: 'Авторизация', exact: false},
-    {to: '/quiz-creator', label: 'Создать тест', exact: false}
-]
-
-
 class Drawer extends Component {
 
     clickHandler = () => {
         this.props.onClose();
     }
 
-    renderLinks(){
+    renderLinks(links) {
         return links.map((link, index) => {
             return (
                 <li key={index}>
@@ -32,15 +25,27 @@ class Drawer extends Component {
     }
 
     render() {
-    const cls = [classes.Drawer]
-    if(!this.props.isOpen){
-        cls.push(classes.close)
-    }
+        const cls = [classes.Drawer]
+        if (!this.props.isOpen) {
+            cls.push(classes.close)
+        }
+
+        const links = [
+            {to: '/', label: 'Список', exact: true}
+        ]
+
+        if (this.props.isAuthenticated) {
+            links.push({to: '/quiz-creator', label: 'Создать тест', exact: false})
+            links.push({to: '/logout', label: 'Выйти', exact: false})
+        } else  {
+            links.push({to: '/auth', label: 'Авторизация', exact: false})
+        }
+
         return (
             <React.Fragment>
                 <nav className={cls.join(' ')}>
                     <ul>
-                        {this.renderLinks()}
+                        {this.renderLinks(links)}
                     </ul>
                 </nav>
                 {this.props.isOpen ? <Backdrop onClick={this.props.onClose}/> : null}
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -45,6 +45,10 @@ export function logout() {
     }
 }
 
+export function autoLogin() {
+
+}
+
 export function authSuccess(token) {
     return {
         type: AUTH_SUCCESS,
